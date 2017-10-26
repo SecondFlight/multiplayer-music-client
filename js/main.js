4ab0dd4f6d76730ba933f41d697040b7ef807734
Joshua Wade <wadej@southern.edu>
2017-10-26 04:51:34 +0000

    Piano now plays with typing keyboard

--- js/main.js
+++ js/main.js
@@ -12,11 +12,51 @@ let patches = [
 	}
 ]
 
+let keyboardMap = {
+	"q":0,
+	"2":1,
+	"w":2,
+	"3":3,
+	"e":4,
+	"r":5,
+	"5":6,
+	"t":7,
+	"6":8,
+	"y":9,
+	"7":10,
+	"u":11,
+	"i":12,
+	"9":13,
+	"o":14,
+	"0":15,
+	"p":16,
+	"[":17,
+	"=":18,
+	"]":19,
+	"/":4,
+	";":3,
+	".":2,
+	"l":1,
+	",":0,
+	"m":-1,
+	"j":-2,
+	"n":-3,
+	"h":-4,
+	"b":-5,
+	"g":-6,
+	"v":-7,
+	"c":-8,
+	"d":-9,
+	"x":-10,
+	"s":-11,
+	"z":-12
+}
+
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 // Webkit/blink browsers need prefix, Safari won't work without window
 // code examples are nice
 
-let sources = {};
+let buffers = {};
 
 // Load the audio files
 patches.forEach(function(patch) {
@@ -28,20 +68,76 @@ patches.forEach(function(patch) {
 		request.responseType = 'arraybuffer';
 
 		request.onload = function() {
-			let audioData = request.response;
+			let dataBuffer = request.response;
 
-			let source = audioCtx.createBufferSource();
-			audioCtx.decodeAudioData(audioData).then(function(buffer) {
-				source.buffer = buffer;
-
-				source.connect(audioCtx.destination);
-
-				// play the audio woot
-				source.start(0);
+			audioCtx.decodeAudioData(dataBuffer, function(buffer) {
+				buffers[layer.filename] = buffer;
 			});
-			sources[layer.filename] = source;
 		};
 
 		request.send();
 	});
 });
+
+let playnote = function(audioFile, coarseDetune) {
+	let source = audioCtx.createBufferSource();
+	source.buffer = buffers[audioFile];
+	source.connect(audioCtx.destination);
+	source.detune.value = 100*coarseDetune;
+	source.start(0);
+}
+
+
+// modified from https://stackoverflow.com/a/10467137/8166701
+function KeyListener(){
+    this.keysStatus = Array();
+    
+    //Constructor
+    $(document).keydown(
+        function(e){
+            e.preventDefault();
+            KeyListener.onKeyDown(e);
+        }
+    );
+    $(document).keyup(
+        function(e){
+            e.preventDefault();
+            KeyListener.onKeyUp(e);
+        }
+    );
+    
+    this.onKeyDown = function(e){
+        var keyCode = e.keyCode;
+        if(this.keysStatus[keyCode]){
+            //consoleOutput('Key ['+keyCode+'] Hold <br/>');
+        } else {
+            this.keysStatus[keyCode] = new Date();
+            
+            const keyName = e.key;
+            const keyNum = keyboardMap[keyName];
+			if (keyNum != undefined) {
+				playnote("audio/piano.wav", keyNum);
+			}
+
+            //this.detectCombinations();
+        }
+    }
+    
+    this.onKeyUp = function(e){
+        var keyCode = e.keyCode;
+        var keyDownDate = this.keysStatus[keyCode];
+        var keyUpDate = new Date();
+        var keyHoldTime = keyUpDate-keyDownDate;
+        this.keysStatus[keyCode] = false;
+
+        // do things maybe
+    }
+    
+    /*this.detectCombinations = function(){
+        if(this.keysStatus[17] && this.keysStatus[66]){
+            consoleOutput('Combination CTRL+B detected <br/>');
+        }
+    }*/
+}
+
+var KeyListener = new KeyListener();
