4ec57a68a28b503981dc97585edb6c48bc2bac36
Joshua Wade <wadej@southern.edu>
2017-11-14 01:12:42 +0000

    Add comment to main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
 let keyboardMap = {"0":87,"2":73,"3":75,"5":78,"6":80,"7":82,"9":85,"q":72,"w":74,"e":76,"r":77,"t":79,"y":81,"u":83,"i":84,"o":86,"p":88,"[":89,"=":90,"]":91,"/":76,";":75,".":74,"l":73,",":72,"m":71,"j":70,"n":69,"h":68,"b":67,"g":66,"v":65,"c":64,"d":63,"x":62,"s":61,"z":60}
 
 let engine = new Engine();
+/*
+// copy from https://stackoverflow.com/questions/17015019/keylistener-in-javascript
+window.onkeydown = (e) => {
+	const keyName = e.key;
+	const keyNum = keyboardMap[keyName];
+	
+	if (keyNum != undefined) 
+	{
+		engine.playNote("Piano", keyNum);
+		
+	}
+}*/
+
 
 // modified from https://stackoverflow.com/a/10467137/8166701
 function KeyListener(){
@@ -56,3 +69,4 @@ function KeyListener(){
 }
 
 var KeyListener = new KeyListener();
+
