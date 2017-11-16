718903ab307a7aad90264c890c276e06b8f8e30e
Joshua Wade <wadej@southern.edu>
2017-11-16 03:10:04 +0000

    I made the keyboard clickable and send ping to the server

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Maps keys on typing keyboard to midi notes
 let keyboardMap = {"0":87,"2":73,"3":75,"5":78,"6":80,"7":82,"9":85,"q":72,"w":74,"e":76,"r":77,"t":79,"y":81,"u":83,"i":84,"o":86,"p":88,"[":89,"=":90,"]":91,"/":76,";":75,".":74,"l":73,",":72,"m":71,"j":70,"n":69,"h":68,"b":67,"g":66,"v":65,"c":64,"d":63,"x":62,"s":61,"z":60}
 
+var socket = io("music.atoms.one:3000");
+
 let engine = new Engine();
 /*
 // copy from https://stackoverflow.com/questions/17015019/keylistener-in-javascript
@@ -14,6 +16,7 @@ window.onkeydown = (e) => {
 		
 	}
 }*/
+
 
 
 // modified from https://stackoverflow.com/a/10467137/8166701
@@ -44,8 +47,9 @@ function KeyListener(){
             const keyName = e.key;
             const keyNum = keyboardMap[keyName];
 			if (keyNum != undefined) {
-				engine.playNote("Piano", keyNum);
+				engine.noteOn("userName", keyNum);
 			}
+			pingServer("userName", keyNum, 'note on', "", 1.0);
 
             //this.detectCombinations();
         }
@@ -68,5 +72,26 @@ function KeyListener(){
     }*/
 }
 
+var noteKeys = document.getElementsByClassName("note");
+for (var i = 0; i < noteKeys.length; i++)
+{
+	var element = noteKeys[i];
+	element.addEventListener("click", function(event) {
+		engine.noteOn("userID", parseInt(event.target.dataset.notenumber));
+	});
+	
+	element.addEventListener("mouseUp", function(event) {
+		engine.noteOn("userID", parseInt(event.target.dataset.notenumber));
+	});
+}
+
 var KeyListener = new KeyListener();
+
+function pingServer(userID, noteNumber, msgType, instrument, velocity){
+	socket.emit(msgType, { 
+		userID: userID,
+		noteNumber: noteNumber,
+		instrument: instrument,
+		velocity: velocity} );
+}
 
