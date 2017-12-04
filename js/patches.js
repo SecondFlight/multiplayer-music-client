6938fd8d1806c7237d2335a6de86cecd143a4be7
Joshua Wade <wadej@southern.edu>
2017-12-04 00:31:01 +0000

    Add a drum kit

---
+++ js/patches.js
@@ -0,0 +1,217 @@
+function GetPatches() {
+	return {
+		"Piano": {
+			"layers": [
+				{
+					"filename": "audio/pianoC2.ogg",
+					"startNote": 0,
+					"endNote": 39,
+					"centerNote": 36
+				},
+				{
+					"filename": "audio/pianoG2.ogg",
+					"startNote": 40,
+					"endNote": 45,
+					"centerNote": 43
+				},
+				{
+					"filename": "audio/pianoC3.ogg",
+					"startNote": 46,
+					"endNote": 51,
+					"centerNote": 48
+				},
+				{
+					"filename": "audio/pianoG3.ogg",
+					"startNote": 52,
+					"endNote": 57,
+					"centerNote": 55
+				},
+				{
+					"filename": "audio/pianoC4.ogg",
+					"startNote": 58,
+					"endNote": 63,
+					"centerNote": 60
+				},
+				{
+					"filename": "audio/pianoG4.ogg",
+					"startNote": 64,
+					"endNote": 69,
+					"centerNote": 67
+				},
+				{
+					"filename": "audio/pianoC5.ogg",
+					"startNote": 70,
+					"endNote": 75,
+					"centerNote": 72
+				},
+				{
+					"filename": "audio/pianoG5.ogg",
+					"startNote": 76,
+					"endNote": 81,
+					"centerNote": 79
+				},
+				{
+					"filename": "audio/pianoC6.ogg",
+					"startNote": 82,
+					"endNote": 87,
+					"centerNote": 84
+				},
+				{
+					"filename": "audio/pianoG6.ogg",
+					"startNote": 88,
+					"endNote": 93,
+					"centerNote": 91
+				},
+				{
+					"filename": "audio/pianoC7.ogg",
+					"startNote": 94,
+					"endNote": 99,
+					"centerNote": 96
+				},
+				{
+					"filename": "audio/pianoG7.ogg",
+					"startNote": 100,
+					"endNote": 105,
+					"centerNote": 103
+				},
+				{
+					"filename": "audio/pianoC8.ogg",
+					"startNote": 106,
+					"endNote": 127,
+					"centerNote": 108
+				}
+			],
+			"envelope": {
+				attackTime: 0,
+				decayTime: 0,
+				sustainLevel: 1,
+				releaseTime: 0.5,
+				curve: "exponential"
+			}
+		},
+		"Sine Bell": {
+			"layers": [
+				{
+					"filename": "audio/sineC4.ogg",
+					"startNote": 0,
+					"endNote": 127,
+					"centerNote": 72,
+					"loop": true
+				}
+			],
+			"envelope": {
+				attackTime: 0,
+				decayTime: 3,
+				sustainLevel: 0.9,
+				releaseTime: 7,
+				curve: "exponential"
+			}
+		},
+		"Drum Kit": {
+			"layers": [
+				{
+					"filename": "audio/Jazz Kick 1.ogg",
+					"startNote": 72,
+					"endNote": 72,
+					"centerNote": 72
+				},
+				{
+					"filename": "audio/Jazz Kick 2.ogg",
+					"startNote": 73,
+					"endNote": 73,
+					"centerNote": 73
+				},
+				{
+					"filename": "audio/Jazz Snare Hit.ogg",
+					"startNote": 74,
+					"endNote": 74,
+					"centerNote": 74
+				},
+				{
+					"filename": "audio/Jazz Snare Rim.ogg",
+					"startNote": 75,
+					"endNote": 75,
+					"centerNote": 75
+				},
+				{
+					"filename": "audio/Jazz Snare Side.ogg",
+					"startNote": 76,
+					"endNote": 76,
+					"centerNote": 76
+				},
+				{
+					"filename": "audio/Jazz Closed HH.ogg",
+					"startNote": 77,
+					"endNote": 77,
+					"centerNote": 77
+				},
+				{
+					"filename": "audio/Jazz Open HH.ogg",
+					"startNote": 78,
+					"endNote": 78,
+					"centerNote": 78
+				},
+				{
+					"filename": "audio/Jazz Half HH.ogg",
+					"startNote": 79,
+					"endNote": 79,
+					"centerNote": 79
+				},
+				{
+					"filename": "audio/Jazz Pedal HH.ogg",
+					"startNote": 80,
+					"endNote": 80,
+					"centerNote": 80
+				},
+				{
+					"filename": "audio/Jazz Crash.ogg",
+					"startNote": 81,
+					"endNote": 81,
+					"centerNote": 81
+				},
+				{
+					"filename": "audio/Jazz Ride.ogg",
+					"startNote": 82,
+					"endNote": 82,
+					"centerNote": 82
+				},
+				{
+					"filename": "audio/Jazz Splash.ogg",
+					"startNote": 83,
+					"endNote": 83,
+					"centerNote": 83
+				},
+				{
+					"filename": "audio/Jazz Tom 4.ogg",
+					"startNote": 84,
+					"endNote": 84,
+					"centerNote": 84
+				},
+				{
+					"filename": "audio/Jazz Tom 3.ogg",
+					"startNote": 85,
+					"endNote": 85,
+					"centerNote": 85
+				},
+				{
+					"filename": "audio/Jazz Tom 2.ogg",
+					"startNote": 86,
+					"endNote": 86,
+					"centerNote": 86
+				},
+				{
+					"filename": "audio/Jazz Tom 1.ogg",
+					"startNote": 87,
+					"endNote": 87,
+					"centerNote": 87
+				}
+			],
+			"envelope": {
+				attackTime: 0,
+				decayTime: 0,
+				sustainLevel: 1,
+				releaseTime: 7
+			}
+		}
+	}
+}
